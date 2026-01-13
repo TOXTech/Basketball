a532c235fe3ef36653590fb2cd3cb321897063ea
TOXTech <toxtechindustry@gmail.com>
2026-01-13 18:29:49 +0200

    third commirt

--- app.js
+++ app.js
@@ -7,26 +7,32 @@ let gameWinEl = document.getElementById("gameWin");
 const add1tohome = () => {
   scoreHome += 1;
   scoreHomeEl.textContent = scoreHome;
+  gameWinEl.textContent = "";
 };
 const add2tohome = () => {
   scoreHome += 2;
   scoreHomeEl.textContent = scoreHome;
+  gameWinEl.textContent = "";
 };
 const add3tohome = () => {
   scoreHome += 3;
   scoreHomeEl.textContent = scoreHome;
+  gameWinEl.textContent = "";
 };
 const add1toguest = () => {
   scoreGuest += 1;
   scoreGuestEl.textContent = scoreGuest;
+  gameWinEl.textContent = "";
 };
 const add2toguest = () => {
   scoreGuest += 2;
   scoreGuestEl.textContent = scoreGuest;
+  gameWinEl.textContent = "";
 };
 const add3toguest = () => {
   scoreGuest += 3;
   scoreGuestEl.textContent = scoreGuest;
+  gameWinEl.textContent = "";
 };
 const resetScores = () => {
   if (scoreHome > scoreGuest) {
